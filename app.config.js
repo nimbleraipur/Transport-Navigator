a22d3f137147de526b962bc69ff064631ed784c5
mdrazachouhan <razamohammad9926@gmail.com>
2026-09-19 17:48:25 +0530

    chore: bump version to 1.0.9 and versionCode to 16 for next Play Store release

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ module.exports = ({ config }) => ({
   owner: "nimbleraipur",
   name: IS_DRIVER ? 'My Load Driver' : 'My Load 24',
   slug: 'md-raza-chouhan', // Must match the project ID on Expo dashboard
-  version: '1.0.8',
+  version: '1.0.9',
   orientation: 'portrait',
   icon: IS_DRIVER ? './assets/images/Myload_Driver.png' : './assets/images/icon.png',
   scheme: IS_DRIVER ? 'myload24driver' : 'myload24',
@@ -28,7 +28,7 @@ module.exports = ({ config }) => ({
   android: {
     package: IS_DRIVER ? 'com.nimble.myload24.driver' : 'com.nimble.myload24',
     googleServicesFile: IS_DRIVER ? './google-services-driver.json' : './google-services-customer.json',
-    versionCode: 15,
+    versionCode: 16,
     permissions: [
       "CAMERA",
       "READ_EXTERNAL_STORAGE",
